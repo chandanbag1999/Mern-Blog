aa1489f407dbc5abe1e7aaff6c535ea4f9468a75
Chandan Bag <cbag67612@gmail.com>
2024-04-28 21:13:48 +0530

    create signUp api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,45 @@
-import User from "../models/user.model.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model.js"
+
 
 
 export const test = (req, res) => {
     res.json({meassage: "hello world"})
 }
 
+export const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if ([ username, email, password ].some((field) => field?.trim() === "")) {
+        throw new ApiError("Please provide all required fields", 400);
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        throw new ApiError("User with this username or email already exists", 400);
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        password: hashedPassword,
+    })
+
+    const newUser = await User.findById(user._id).select("-password");
+
+    if (!newUser) {
+        throw new ApiError("Failed to create user", 500);
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, "User created successfully", newUser));
+})
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { test } from "../controllers/user.controller.js";
+import { registerUser, test } from "../controllers/user.controller.js";
 
 
 const router = Router();
 
 router.route("/test").get(test)
+router.route("/sign-up").post(registerUser)
 
 
 export default router;
